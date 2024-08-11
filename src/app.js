38fd9f8a47f03008e5308a5af598b7d556c531b9
Mich-Tapawan <mik.tapawan@gmail.com>
2024-08-11 22:17:11 +0800

    Added leaflet js library for map

--- src/app.js
+++ src/app.js
@@ -1,1 +1,7 @@
-const searchInput = document.getElementById('search-loc');
+var map = L.map('map').setView([12.8797, 121.7740], 5);
+
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map);
+
