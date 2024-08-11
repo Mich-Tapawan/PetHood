021187c038afe562790fd181b02016aeb6f129e0
Mich-Tapawan <mik.tapawan@gmail.com>
2024-08-11 22:56:43 +0800

    Added variables for map text content

--- src/app.js
+++ src/app.js
@@ -1,3 +1,18 @@
+/* 
+- If search bar is empty, then dont display map
+- Collect user input from search
+- on click, fetch nominatim api with the additional query of ' , Philippines' (use async await)
+- Parse returned json file
+- Display the name of the store, type, and full address to each card
+*/
+
+const searchInput = document.getElementById('search');
+const resultList = document.getElementById('result-list');
+const mapContainer = document.getElementById('map');
+const currentMarkers = [];
+const locName = document.getElementById('loc-name');
+const address = document.getElementById('loc-add');
+
 var map = L.map('map').setView([12.8797, 121.7740], 5);
 
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
