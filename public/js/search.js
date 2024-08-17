1e928290c6fac399aff528297f2e7b8d7fecab44
Mich-Tapawan <mik.tapawan@gmail.com>
2024-08-17 21:45:29 +0800

    Fixed responsive design for card-col in search page

--- public/js/search.js
+++ public/js/search.js
@@ -1,2 +1,33 @@
 document.addEventListener('DOMContentLoaded', ()=>{
+    let screenWidth = window.innerWidth; 
+    let location = document.querySelector('#search');
+    let mapContainer = document.querySelector('.map-container');
+    placeholderChanger();
+    displayMap();   
+
+    window.addEventListener('resize', ()=>{
+        placeholderChanger();
+        displayMap();   
+    });
+
+    // Search Placeholder Changer
+    function placeholderChanger(){
+        screenWidth = window.innerWidth;
+        if(screenWidth < 768){
+            location.placeholder = 'Enter a location...';
+        }
+        else{
+            location.placeholder ='Enter a location (e.g. City, Province, Region)';
+        }
+    }
+
+    function displayMap(){
+        screenWidth = window.innerWidth;
+        if(screenWidth < 768){
+            mapContainer.style.display = 'none';
+        }
+        else{
+            mapContainer.style.display = 'block';
+        }
+    }
 })
