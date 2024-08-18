551103b40df234135348edf61ce707f268800c74
Mich-Tapawan <mik.tapawan@gmail.com>
2024-08-18 10:15:24 +0800

    Removed unused code

--- public/js/search.js
+++ public/js/search.js
@@ -1,13 +1,10 @@
 document.addEventListener('DOMContentLoaded', ()=>{
     let screenWidth = window.innerWidth; 
     let location = document.querySelector('#search');
-    let mapContainer = document.querySelector('.map-container');
     placeholderChanger();
-    displayMap();   
 
     window.addEventListener('resize', ()=>{
         placeholderChanger();
-        displayMap();   
     });
 
     // Search Placeholder Changer
@@ -20,14 +17,4 @@ document.addEventListener('DOMContentLoaded', ()=>{
             location.placeholder ='Enter a location (e.g. City, Province, Region)';
         }
     }
-
-    // function displayMap(){
-    //     screenWidth = window.innerWidth;
-    //     if(screenWidth < 768){
-    //         mapContainer.style.display = 'none';
-    //     }
-    //     else{
-    //         mapContainer.style.display = 'block';
-    //     }
-    // }
 })
